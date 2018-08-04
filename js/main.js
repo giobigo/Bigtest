c6f8a9631028f8b9b785fd85df7d9b7aff7c8fc2
giobigo <32678664+giobigo@users.noreply.github.com>
2018-08-05 02:34:34 +0400

    Update main.js
    I updated it for master :)

--- js/main.js
+++ js/main.js
@@ -16,4 +16,22 @@ alert(surname);
      $('tbody').append("<tr><th scope='row'>"+ number +"</th><td>"+firstname+"</td><td>"+surname+"</td><td><button type='button' class='btn btn-danger delBtn' style='min-width:70%;'>Delete</button></td></tr>");
     });
 
+    $('.edBtn').on('click', function(e){
+
+      var number1 = $('.tdNumber').html();
+      var firstname1 = $('.tdFirstame').html();
+      var surname1 = $('.tdSurname').html();
+alert(surname1);
+      $('#modalNumber').val(number1);
+      $('#modalFirstname').val(firstname1);
+      $('#modalSurname').val(surname1);
+
+
+    })
+
+
+
+
+
+
 });
