90fbc16c779ef2060a23da1ce570297fafba7a60
gio-PC\gio <giorgi.bigvava07@geolab.edu.ge>
2018-08-04 20:31:27 +0400

    Person add is solved and edit button added in front page.

--- js/main.js
+++ js/main.js
@@ -2,12 +2,18 @@ $(document).ready(function(){
 
 
     $(document).on('click','.delBtn', function(e){
-        $(this).parents("tr").hide();
+        $(this).parents("tr").hide(1000);
+
     });
 
 
     $('.addBtn').on( 'click', function(e) {
-      $('tbody').append("<tr><th scope='row'>"+ $($(this),'nNumber').val()+"</th><td>"+$($(this),'Firstname').val()+"</td><td>"+$($(this),'Surname').val()+"</td><td><button type='button' class='btn btn-danger delBtn' style='min-width:70%;'>Delete</button></td></tr>");
+      var number = $('#Number').val();
+      var firstname = $('#Firstname').val();
+      var surname = $('#Lastname').val();
+
+alert(surname);
+     $('tbody').append("<tr><th scope='row'>"+ number +"</th><td>"+firstname+"</td><td>"+surname+"</td><td><button type='button' class='btn btn-danger delBtn' style='min-width:70%;'>Delete</button></td></tr>");
     });
 
 });
